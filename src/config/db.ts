7b82b0b2cc543a102619ca6507071005957905b1
Frank-Totti <juanfrancescogarcia@gmail.com>
2025-11-18 21:32:12 -0500

    deleted firebase name

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,5 +1,5 @@
 import admin from "firebase-admin";
-import serviceAccount from "./charlaton-backend-firebase-adminsdk-fbsvc-49794478e4.json";
+import serviceAccount from "./firebase.json";
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount as admin.ServiceAccount)
